995650f64ab2d83a4168a76e5c55ae6b2f0cfc60
Abeer Tannous <{abeer.tannous}@yahoo.com>
2021-11-26 08:01:44 -0500

    installing inquirer

--- develop/utils/index.js
+++ develop/utils/index.js
@@ -1,14 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = inquirer.prompt([
-    {
-      type: 'input',
-      name: 'Title',
-      message: 'Waht is you project Tilte',
-    }
-
-]) ;
+const questions = [];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
